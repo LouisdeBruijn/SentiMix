71f7399ef830d34992aa01a298531a8e75ecbf74
Toki Wong <wongtoki5@gmail.com>
2019-10-15 10:15:09 +0200

    Embedding test

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import tools.baseline as baseline_tools
 import numpy as np
 
 from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 
-dt_path = "./data_files/spanglish_trial.txt"
+dt_path = "./data_files/train_conll_spanglish.txt"
 dt_embeddings = "./data_files/embeddings.json"
 
 data = data_tools.Data(dt_embeddings, dt_path, shuffle=True, split=0.7)
-baseline_tools.test_one(KNeighborsClassifier(),data)
+baseline_tools.test_one(DecisionTreeClassifier(),data)
 # baseline_tools.plot_svm_accuracy(data)
 # vec = baseline_tools.word_embedding_vectorizor(['hello','world'], dt_embeddings)
 
