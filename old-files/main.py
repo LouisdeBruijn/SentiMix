4c0ee531be6e9f756527373fd24b05248739dd40
Gaetana Ruggiero <garuggiero@gmail.com>
2019-12-02 22:48:01 +0100

    Linear SVC used in order to use ovr approach

--- old-files/main.py
+++ old-files/main.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import tools.baseline as bt
 import tools.data as data_tools
 import tools.baseline as baseline_tools
@@ -7,13 +7,12 @@ import numpy as np
 from sklearn.svm import LinearSVC, SVC
 from sklearn.neighbors import KNeighborsClassifier
 
-dt_path = "../data_files/train_conll_hinglish.txt"
+dt_path = "../data_files/train_conll_spanglish.txt"
 dt_embeddings = ["../data_files/wiki.en.align.vec", "../data_files/wiki.es.align.vec"]
 
 data = data_tools.Data(dt_embeddings, dt_path, shuffle=True, split=0.7)
 
 
-baseline_tools.test_one(SVC(kernel="linear"),data)
+baseline_tools.test_one(LinearSVC(),data)
 # baseline_tools.plot_svm_accuracy(data)
 # vec = baseline_tools.word_embedding_vectorizor(['hello','world'], dt_embeddings)
-
