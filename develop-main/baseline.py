837bcbc7da06c53faf738b57cd008cc7476f0eb6
wongtoki5@gmail.com <wongtoki5@gmail.com>
2020-01-28 19:43:35 +0100

    testing

--- develop-main/baseline.py
+++ develop-main/baseline.py
@@ -153,8 +153,8 @@ if __name__ == "__main__":
     train_conll = Data(
         "../data_files/final_train.conll", format="conll")
 
-    # train_2016 = Data(
-    #     "../data_files/2016_spanglish_annotated.json", format="json")
+    train_2016 = Data(
+        "../data_files/2016_spanglish_annotated.json", format="json")
 
     test = Data("../data_files/final_trial.conll", format="conll")
 
@@ -166,23 +166,23 @@ if __name__ == "__main__":
 
     # run_baseline_tfidf(data_set[0], data_set[1])
 
-    # from gensim.models import KeyedVectors
-    # # emb_en = KeyedVectors.load_word2vec_format(
-    # #     "../data_files/wiki.en.align.vec")
-    # # emb_es = KeyedVectors.load_word2vec_format(
-    # #     "../data_files/wiki.es.align.vec")
+    from gensim.models import KeyedVectors
+    emb_en = KeyedVectors.load_word2vec_format(
+        "../data_files/wiki.en.align.vec")
+    emb_es = KeyedVectors.load_word2vec_format(
+        "../data_files/wiki.es.align.vec")
 
     # google = KeyedVectors.load_word2vec_format(
     #     "../data_files/GoogleNews-vectors-negative300.bin", binary=True)
 
-    # # embs = [emb_en, emb_es]
+    embs = [emb_en, emb_es]
     # embs = [google]
 
-    # print("\nWord embeddings using final_train.conll")
-    # run_baseline_embeddings(train_conll, test, embs)
+    print("\nWord embeddings using final_train.conll")
+    run_baseline_embeddings(train_conll, test, embs)
 
-    # print("\nWord embeddings using 2016_spanglish")
-    # run_baseline_embeddings(train_2016, test, embs)
+    print("\nWord embeddings using 2016_spanglish")
+    run_baseline_embeddings(train_2016, test, embs)
 
     # Enable a console for realtime testing
     # console(model)
